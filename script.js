ff106b2a1c5fe863345c70b35fdd7929c7ed17df
Najoua Bentaher <117578236+NajouaBentaher@users.noreply.github.com>
2023-08-29 10:22:57 +0100

    Switch the form to an alert

--- script.js
+++ script.js
@@ -1,29 +1,51 @@
-let table = document.getElementsByClassName("sheet-body")[0],
-rows = document.getElementsByClassName("rows")[0],
-columns = document.getElementsByClassName("columns")[0]
-tableExists = false
+let alertEl = document.createElement("div");
+alertEl.className = "alert";
+let rowsInp = document.createElement("input");
+rowsInp.type = "number";
+rowsInp.placeholder = "Enter number of rows";
+rowsInp.className = "rowsInp";
+
+let columnsInp = document.createElement("input");
+columnsInp.type = "number";
+columnsInp.placeholder = "Enter number of columns";
+columnsInp.className = "columnsInp";
+
+alertEl.appendChild(rowsInp);
+alertEl.appendChild(columnsInp);
+
+let table = document.getElementsByClassName("sheet-body")[0];
+let okGenBtn = document.getElementsByClassName("swal-button");
+tableExists = false;
 
 const generateTable = () => {
-    let rowsNumber = parseInt(rows.value), columnsNumber = parseInt(columns.value)
-    table.innerHTML = ""
-    for(let i=0; i<rowsNumber; i++){
-        var tableRow = ""
-        for(let j=0; j<columnsNumber; j++){
-            tableRow += `<td contenteditable></td>`
-        }
-        table.innerHTML += tableRow
+  swal({ content: alertEl });
+  console.log(okGenBtn[0]);
+
+  okGenBtn[0].onclick = () => {
+    let rowsNumber = parseInt(rowsInp.value),
+      columnsNumber = parseInt(columnsInp.value);
+    table.innerHTML = "";
+    for (let i = 0; i < rowsNumber; i++) {
+      var tableRow = "";
+      for (let j = 0; j < columnsNumber; j++) {
+        tableRow += `<td contenteditable></td>`;
+      }
+      table.innerHTML += tableRow;
     }
-    if(rowsNumber>0 && columnsNumber>0){
-        tableExists = true
+    if (rowsNumber > 0 && columnsNumber > 0) {
+      tableExists = true;
     }
-}
+  };
+};
 
 const ExportToExcel = (type, fn, dl) => {
-    if(!tableExists){
-        return
-    }
-    var elt = table
-    var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" })
-    return dl ? XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' })
-        : XLSX.writeFile(wb, fn || ('MyNewSheet.' + (type || 'xlsx')))
-}
+  if (!tableExists) {
+    generateTable();
+    return;
+  }
+  var elt = table;
+  var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
+  return dl
+    ? XLSX.write(wb, { bookType: type, bookSST: true, type: "base64" })
+    : XLSX.writeFile(wb, fn || "MyNewSheet." + (type || "xlsx"));
+};
